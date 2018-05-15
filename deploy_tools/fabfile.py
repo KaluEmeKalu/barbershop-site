5d9cd0e1d5ff8b63029f7177fbde3d7938ecc118
Kalu Kalu <kalu@kaluekalu.com>
2018-05-15 22:14:01 +0800

    changed github fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = 'https://github.com/Leviona/barbershop-site'  
+REPO_URL = 'git@github.com:KaluEmeKalu/barbershop-site.git'  
 
 
 def _get_latest_source():
